937199a473e1ebfbdc261a76cdb4a090d6218c5f
ShanEvgeny <saevgenij76@gmail.com>
2025-12-13 21:28:15 +0800

    1.0.5

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Genre, MovieHouse, Director, Movie, MovieHall, MovieSession, Ticket
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title','description']
+@admin.register(MovieHouse)
+class MovieHouseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title','address','phone_number']
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name','date_of_birth']
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title','description','movie_running_time']
+    filter_horizontal = ['directors','genres']
+    pass
+@admin.register(MovieHall)
+class MovieHallAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie_house','sequence_number','sequence_number','count_of_seats']
+    pass
+@admin.register(MovieSession)
+class MovieSessionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie_hall','movie','date_of_event']
+    pass
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie_session','count_of_seats_purchased']
+    pass
--- movies/models.py
+++ movies/models.py
@@ -1,2 +1,62 @@
 from django.db import models
 
+class Genre(models.Model):
+    title = models.TextField("Название")
+    description = models.TextField("Описание")
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+    def __str__(self) -> str:
+        return self.title
+class MovieHouse(models.Model):
+    title = models.TextField("Название")
+    address = models.TextField("Адрес")
+    phone_number = models.TextField("Номер телефона")
+    class Meta:
+        verbose_name = "Кинотеатр"
+        verbose_name_plural = "Кинотеатры"
+    def __str__(self) -> str:
+        return self.title
+class Director(models.Model):
+    full_name = models.TextField("Полное имя")
+    date_of_birth  = models.DateField("Дата рождения")
+    class Meta:
+        verbose_name = "Режиссер"
+        verbose_name_plural = "Режиссеры"
+    def __str__(self) -> str:
+        return self.full_name
+class Movie(models.Model):
+    title = models.TextField("Название")
+    description = models.TextField("Описание фильма")
+    movie_running_time = models.DurationField("Хронометраж фильма")
+    directors = models.ManyToManyField('Director', related_name='movies_directors',verbose_name="Режиссеры")
+    genres = models.ManyToManyField('Genre', related_name='movies_genres',verbose_name="Жанры")
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
+    def __str__(self) -> str:
+        return self.title
+class MovieHall(models.Model):
+    movie_house =  models.ForeignKey('MovieHouse', on_delete = models.CASCADE,verbose_name='Кинотеатр', null = True)
+    sequence_number = models.PositiveIntegerField("Порядковый номер зала")
+    count_of_seats = models.PositiveIntegerField("Количество мест")
+    class Meta:
+        verbose_name = "Кинозал"
+        verbose_name_plural = "Кинозалы"
+        unique_together = ['movie_house', 'sequence_number']
+class MovieSession(models.Model):
+    #movie_house = models.ForeignKey('MovieHouse', on_delete = models.CASCADE,verbose_name='Кинотеатр', null = True)
+    movie_hall = models.ForeignKey('MovieHall', on_delete = models.CASCADE,verbose_name='Зал', null = True)
+    movie = models.ForeignKey('Movie', on_delete = models.CASCADE,verbose_name='Фильм', null = True)
+    date_of_event = models.DateTimeField("Дата проведения")
+    class Meta:
+        verbose_name = "Киносеанс"
+        verbose_name_plural = "Киносеансы"
+class Ticket(models.Model):
+    movie_session = models.ForeignKey('MovieSession', on_delete = models.CASCADE,verbose_name='Сеанс', null = True)
+    count_of_seats_purchased = models.PositiveIntegerField("Цена")
+    class Meta:
+        verbose_name = "Билет"
+        verbose_name_plural = "Билеты"
+        
+    
